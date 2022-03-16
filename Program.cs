5413a0269d2f328856ad167254cc5cd394655900
OuroborosCaster <yangyanpei20000308@outlook.com>
2022-03-16 16:34:24 +0800

    添加文本文档，word与excel生成功能

--- Program.cs
+++ Program.cs
@@ -10,15 +10,15 @@ namespace CovidDataApp
     {
         static void Main(string[] args)
         {
-            ParseData covid=new ParseData();
+            ParseData covid = new ParseData();
             covid.ParseJSON();
-            string[] Countries=covid.getListCountry();
-            string[] Continents=covid.getListContinent();
+            string[] Countries = covid.getListCountry();
+            string[] Continents = covid.getListContinent();
             string[] Populations = covid.getListPopulation();
             string[] Days = covid.getListDay();
-            string[] Times=covid.getListTime();
+            string[] Times = covid.getListTime();
 
-            string[] NewCases=covid.getListNewCases();
+            string[] NewCases = covid.getListNewCases();
             string[] ActiveCases = covid.getListActiveCases();
             string[] CriticalCases = covid.getListCriticalCases();
             string[] RecoveredCases = covid.getListRecoveredCases();
@@ -32,8 +32,11 @@ namespace CovidDataApp
             string[] TestsM_pop = covid.getTestsM_pop();
             string[] TotalTests = covid.getListTotalTests();
 
-            int size= Countries.Length;
-            string[] lines=new string[size];
+            int size = Countries.Length;
+
+            #region 控制台与文本文档内容
+            string[] lines = new string[size];
+
             for (int i = 0; i < size; i++)
             {
                 int index = i + 1;
@@ -41,14 +44,68 @@ namespace CovidDataApp
                 lines[i] = $"|{index.ToString().PadRight(3)}| {Countries[i].PadRight(22)}| {Continents[i].PadRight(14)}| {Populations[i].PadRight(10)}| {NewCases[i].PadRight(7)}| {ActiveCases[i].PadRight(10)}| {CriticalCases[i].PadRight(8)}| {RecoveredCases[i].PadRight(9)}|{TotalCases[i].PadRight(10)}| {NewDeaths[i].PadRight(7)}| {TotalDeaths[i].PadRight(9)}| {TotalTests[i].PadRight(10)}| {Days[i].PadRight(11)}|";
 
             }
-
-            Console.WriteLine($"查询时间：{DateTime.Now} 数据来源：covid-193.p.rapidapi.com");
-            //Console.WriteLine($"|国家                   |所属洲         |人口       |新增病例|当前病例  |当前重症|已康复  |每百万人口患病数|历史总病例 |新增死亡|每百万人死亡病数           |历史总死亡          |每百万人口检测数        |总检测数          |上次更新时间         |");
-            Console.WriteLine($"|   |国家/地区              |所属洲         |人口       |新增病例|当前病例   |当前重症 |已康复    |历史总病例|新增死亡|历史总死亡|总检测数   |上次更新时间|");
-            foreach (string line in lines) 
-            { 
+            string basic = $"查询时间：{DateTime.Now} 数据来源：covid-193.p.rapidapi.com";
+            Console.WriteLine(basic);
+            string header = $"|   |国家/地区              |所属洲         |人口       |新增病例|当前病例   |当前重症 |已康复    |历史总病例|新增死亡|历史总死亡|总检测数   |上次更新时间|";
+            //string header =$"|国家                   |所属洲         |人口       |新增病例|当前病例  |当前重症|已康复  |每百万人口患病数|历史总病例 |新增死亡|每百万人死亡病数           |历史总死亡          |每百万人口检测数        |总检测数          |上次更新时间         |";
+            Console.WriteLine(header);
+            string body = "";
+            foreach (string line in lines)
+            {
+                body = body + line + "\n";
                 Console.WriteLine($"{line}");
-            }                    
+            }
+            string txtContent = basic + "\n" + header + "\n" + body;
+            //Console.WriteLine(txtContent);
+            WriteInFile.WriteContentInTxt(txtContent);
+
+            #endregion
+
+            #region
+            string[,] table = new string[size + 1, 13];
+
+            for (int i = 0; i <= size - 1; i++)
+            {
+                if (i == 0)
+                {
+                    table[i, 0] = "#";
+                    table[i, 1] = "国家/地区";
+                    table[i, 2] = "所属洲";
+                    table[i, 3] = "人口 ";
+                    table[i, 4] = "新增病例";
+                    table[i, 5] = "当前病例";
+                    table[i, 6] = "当前重症";
+                    table[i, 7] = "已康复";
+                    table[i, 8] = "历史总病例";
+                    table[i, 9] = "新增死亡";
+                    table[i, 10] = "历史总死亡";
+                    table[i, 11] = "总检测数";
+                    table[i, 12] = "上次更新时间";
+                }
+                else
+                {
+                    table[i, 0] = i.ToString();
+                    table[i, 1] = Countries[i - 1];
+                    table[i, 2] = Continents[i - 1];
+                    table[i, 3] = Populations[i - 1];
+                    table[i, 4] = NewCases[i - 1];
+                    table[i, 5] = ActiveCases[i - 1];
+                    table[i, 6] = CriticalCases[i - 1];
+                    table[i, 7] = RecoveredCases[i - 1];
+                    table[i, 8] = TotalCases[i - 1];
+                    table[i, 9] = NewDeaths[i - 1];
+                    table[i, 10] = TotalDeaths[i - 1];
+                    table[i, 11] = TotalTests[i - 1];
+                    table[i, 12] = Days[i - 1];
+                }
+
+            }
+
+            #endregion
+            WriteInFile.WriteContentInDoc(basic, table);
+            WriteInFile.WriteContentInExcel(basic, table);
+
+
             Console.ReadKey();
         }
     }
---
+++ WriteInFile.cs
@@ -0,0 +1,207 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using MSWord = Microsoft.Office.Interop.Word;
+using Microsoft.Office.Core;
+using MSExcel=Microsoft.Office.Interop.Excel;
+
+namespace CovidDataApp
+{
+    class WriteInFile
+    {
+        public static void WriteContentInTxt(String content)
+        {
+            try
+            {
+                string fileName = DateTime.Now.ToString("yyyy年M月d日hh时m分s秒");
+                using (StreamWriter sw = new StreamWriter($"{fileName}.txt"))
+                {
+                    sw.WriteLine(content);
+                }
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+        public static void WriteContentInDoc(string header, string[,] tb)
+        {
+            object path;
+            //string docContent=content;
+            MSWord.Application wordApp; //Word应用程序变量 
+            MSWord.Document wordDoc;    //Word文档变量
+
+            string fileName = DateTime.Now.ToString("yyyy年M月d日hh时m分s秒");
+            path = Environment.CurrentDirectory + @"\" + fileName + ".docx";
+
+            wordApp = new MSWord.ApplicationClass();
+            wordApp.Visible = false;//设置是否前台运行
+
+            if (File.Exists((string)path))
+            {
+                File.Delete((string)path);
+            }
+
+            //由于使用的是COM库，因此有许多变量需要用Missing.Value代替
+            Object Nothing = Missing.Value;
+
+            wordDoc = wordApp.Documents.Add(ref Nothing, ref Nothing, ref Nothing, ref Nothing);
+
+            #region 页面设置
+
+            wordDoc.PageSetup.PaperSize = MSWord.WdPaperSize.wdPaperA4;//设置纸张样式为A4纸
+            wordDoc.PageSetup.Orientation = MSWord.WdOrientation.wdOrientLandscape;//排列方式为垂直方向
+            wordDoc.PageSetup.TopMargin = 57.0f;
+            wordDoc.PageSetup.BottomMargin = 57.0f;
+            wordDoc.PageSetup.LeftMargin = 57.0f;
+            wordDoc.PageSetup.RightMargin = 57.0f;
+            wordDoc.PageSetup.HeaderDistance = 30.0f;//页眉位置            
+
+            #endregion
+
+            #region 页码设置并添加页码
+
+            //为当前页添加页码
+            MSWord.PageNumbers pns = wordApp.Selection.Sections[1].Headers[MSWord.WdHeaderFooterIndex.wdHeaderFooterEvenPages].PageNumbers;//获取当前页的号码
+            pns.NumberStyle = MSWord.WdPageNumberStyle.wdPageNumberStyleNumberInDash;//设置页码的风格，是Dash形还是圆形的
+            pns.HeadingLevelForChapter = 0;
+            //pns.IncludeChapterNumber = false;
+            pns.RestartNumberingAtSection = false;
+            pns.StartingNumber = 1; //开始页页码
+            object pagenmbetal = MSWord.WdPageNumberAlignment.wdAlignPageNumberRight;//将号码设置在右边
+            object first = true;
+            wordApp.Selection.Sections[1].Footers[MSWord.WdHeaderFooterIndex.wdHeaderFooterEvenPages].PageNumbers.Add(ref pagenmbetal, ref first);
+
+            #endregion
+
+            #region 行间距与缩进
+
+            wordApp.Selection.ParagraphFormat.LineSpacing = 16f;//设置文档的行间距
+            wordApp.Selection.ParagraphFormat.FirstLineIndent = 30;//首行缩进的长度            
+
+            #endregion
+
+            #region 写入内容
+            object unit = MSWord.WdUnits.wdStory;
+
+            //写入抬头            
+            wordApp.Selection.EndKey(ref unit, ref Nothing);//将光标移到文本末尾
+            wordApp.Selection.ParagraphFormat.FirstLineIndent = 0;//取消首行缩进的长度            
+            wordDoc.Paragraphs.Last.Range.Font.Name = "黑体";
+            wordDoc.Paragraphs.Last.Range.Text = header;
+
+            //添加表格、填充数据、设置表格行列宽高
+
+            wordDoc.Content.InsertAfter("\n");//这一句与下一句的顺序不能颠倒，原因还没搞透
+            wordApp.Selection.EndKey(ref unit, ref Nothing); //将光标移动到文档末尾
+            wordApp.Selection.ParagraphFormat.Alignment = MSWord.WdParagraphAlignment.wdAlignParagraphCenter;//居中对齐
+            object WdLine2 = MSWord.WdUnits.wdLine;//换一行;  
+            wordApp.Selection.MoveDown(ref WdLine2, 6, ref Nothing);//向下跨15行输入表格，这样表格就在文字下方了，不过这是非主流的方法
+
+            //设置表格的行数和列数
+            int table1Row = 63;
+            int table1Column = 13;
+
+            //定义一个Word中的表格对象
+            MSWord.Table table1 = wordDoc.Tables.Add(wordApp.Selection.Range, table1Row, table1Column, ref Nothing, ref Nothing);
+
+            //默认创建的表格没有边框，这里修改其属性，使得创建的表格带有边框 
+            table1.Borders.Enable = 1;//这个值可以设置得很大，例如5、13等等
+
+            //表格的索引是从1开始的。
+            wordDoc.Tables[1].Cell(1, 1).Range.Text = "列\n行";
+            for (int i = 1; i <= table1Row; i++)
+            {
+                for (int j = 1; j <= table1Column; j++)
+                {
+                    table1.Cell(i, j).Range.Text = tb[i - 1, j - 1];
+
+                }
+            }
+
+
+            //设置table样式
+            table1.Rows.HeightRule = MSWord.WdRowHeightRule.wdRowHeightAtLeast;//高度规则是：行高有最低值下限？
+            table1.Rows.Height = wordApp.CentimetersToPoints(float.Parse("0.8"));// 
+
+            table1.Range.Font.Size = 10.5F;
+            table1.Range.Font.Bold = 0;
+
+            table1.Range.ParagraphFormat.Alignment = MSWord.WdParagraphAlignment.wdAlignParagraphCenter;//表格文本居中
+            table1.Range.Cells.VerticalAlignment = MSWord.WdCellVerticalAlignment.wdCellAlignVerticalBottom;//文本垂直贴到底部
+                                                                                                            //设置table边框样式
+            table1.Borders.OutsideLineStyle = MSWord.WdLineStyle.wdLineStyleDouble;//表格外框是双线
+            table1.Borders.InsideLineStyle = MSWord.WdLineStyle.wdLineStyleSingle;//表格内框是单线
+
+            table1.Rows[1].Range.Font.Bold = 1;//加粗
+            table1.Rows[1].Range.Font.Size = 12F;
+            table1.Cell(1, 1).Range.Font.Size = 10.5F;
+            wordApp.Selection.Cells.Height = 30;//所有单元格的高度
+
+
+            #endregion
+
+            wordApp.Selection.EndKey(ref unit, ref Nothing); //将光标移动到文档末尾
+
+
+            object format = MSWord.WdSaveFormat.wdFormatDocumentDefault;
+
+            wordDoc.SaveAs(ref path, ref format, ref Nothing, ref Nothing, ref Nothing, ref Nothing, ref Nothing, ref Nothing, ref Nothing, ref Nothing, ref Nothing, ref Nothing, ref Nothing, ref Nothing, ref Nothing, ref Nothing);
+            //关闭wordDoc文档对象
+
+            //看是不是要打印
+            //wordDoc.PrintOut();
+
+
+
+            wordDoc.Close(ref Nothing, ref Nothing, ref Nothing);
+            //关闭wordApp组件对象
+            wordApp.Quit(ref Nothing, ref Nothing, ref Nothing);
+            System.Runtime.InteropServices.Marshal.ReleaseComObject(wordApp);
+            Console.WriteLine(path + " 创建完毕！");
+
+
+        }
+
+        public static void WriteContentInExcel(string header, string[,] tb)
+        {
+            Object Nothing = Missing.Value;
+
+            MSExcel.Application excelApp = new MSExcel.Application();//初始化应用
+            MSExcel.Workbooks books=excelApp.Workbooks;
+            MSExcel.Workbook book = books.Add(Nothing);//初始化workbook
+
+            object path;
+            string fileName = DateTime.Now.ToString("yyyy年M月d日hh时m分s秒");
+            path = Environment.CurrentDirectory + @"/" + fileName + ".xlsx";
+
+            
+            excelApp.Visible = false;
+
+            if (File.Exists((string)path))
+            {
+                File.Delete((string)path);
+            }
+
+            MSExcel.Sheets sheets = book.Sheets;
+            MSExcel.Worksheet sheet=(MSExcel.Worksheet)sheets.get_Item(1);//初始化worksheet
+
+            sheet.Cells.NumberFormatLocal = "@";//设置单元格格式为文本
+            sheet.Cells.EntireColumn.AutoFit();
+
+            for (int i = 1; i <= tb.GetLength(0); i++)
+            {
+                for(int j = 1; j <= tb.GetLength(1); j++)
+                {
+                    sheet.Cells[i, j] = tb[i-1, j-1];
+                }
+            }
+
+            book.SaveAs(path, Nothing, Nothing, Nothing, Nothing, Nothing, MSExcel.XlSaveAsAccessMode.xlNoChange, Nothing, Nothing, Nothing, Nothing, Nothing);
+            excelApp.Quit();
+            System.Runtime.InteropServices.Marshal.ReleaseComObject(excelApp);
+            Console.WriteLine(path + " 创建完毕！");
+        }
+    }
+}
